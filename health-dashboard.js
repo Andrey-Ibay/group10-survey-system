42c1d6515d09cffd84f6c8b59172d8ea6021d206
Jhon Michael Estorgio <estorgiojhonmichael@gmail.com>
2026-05-10 21:15:24 +0800

    Health Dash Board Added

--- health-dashboard.js
+++ health-dashboard.js
@@ -6,3 +6,79 @@ const date = today.toLocaleDateString('en-US', {
 });
 
 document.querySelector(".today-date").innerText = date;
+
+const demoButton = document.querySelector(".demographics-button");
+const healthButton = document.querySelector(".health-status-button");
+const demoContainer = document.querySelector(".question-container-demo");
+const healthContainers = document.querySelectorAll(".question-container");
+
+const toggleActiveDemo = () => {
+    demoButton.style.background = "#36B249";
+    demoButton.style.color = "white";
+    
+    healthButton.style.background = "white";
+    healthButton.style.color = "black";
+
+    demoContainer.style.display = "grid"
+    healthContainers.forEach(div => div.style.setProperty("display", "none", "important"));
+}
+
+const toggleActiveHealth = () => {
+    demoButton.style.background = "white";
+    demoButton.style.color = "black";
+    
+    healthButton.style.background = "#36B249";
+    healthButton.style.color = "white";
+
+    demoContainer.style.display = "none"
+    healthContainers.forEach(div => div.style.setProperty("display", "grid", "important"));
+}
+
+const healthNavButton = document.querySelector(".health-button");
+const vendorNavButton = document.querySelector(".vendor-button");
+const healthIcon = document.querySelector(".health-icon");
+const vendorIcon = document.querySelector(".vendor-icon");
+const healthDashboard = document.querySelector(".health-dashboard-container");
+
+const healthNavActive = () => {
+    healthNavButton.style.background = "#36B249";
+    vendorNavButton.style.background = "white";
+
+    healthNavButton.style.color = "white";
+    vendorNavButton.style.color = "black";
+
+    healthIcon.src = "assets/health-icon-active.png";
+    vendorIcon.src = "assets/vendor-icon.png";
+
+    healthDashboard.style.display = ""
+}
+
+const vendorNavActive = () => {
+    healthNavButton.style.background = "white";
+    vendorNavButton.style.background = "#36B249";
+
+    healthNavButton.style.color = "black";
+    vendorNavButton.style.color = "white";
+
+    healthIcon.src = "assets/health-icon.png";
+    vendorIcon.src = "assets/vendor-icon-active.png";
+
+    healthDashboard.style.display = "none"
+}
+
+const cancelButton = document.querySelector(".cancel-button");
+const yesButton = document.querySelector(".yes-logout-button");
+const logoutConfirmContainer = document.querySelector(".logout-confirm-container");
+const logoutButton = document.querySelector(".log-out-btn");
+
+const toggleLogOut = () => {
+    logoutConfirmContainer.style.display = "grid";
+}
+
+const cancelLogOut = () => {
+    logoutConfirmContainer.style.display = "none";
+}
+
+const confirmLogOut = () => {
+    window.location.href = "homepage.html";
+}
